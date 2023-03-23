ddf4283eee43e868fe1020216a5a66986bb37082
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-03-23 22:00:03 +0630

    fungsi data for 2

--- InsertionSort3/InsertionSort3.cpp
+++ InsertionSort3/InsertionSort3.cpp
@@ -25,9 +25,22 @@ void input()
 	cout << "Masukan Element Array" << endl;
 	cout << "=====================" << endl;
 
-	for (int i = 0 < n; i++)   //Menggunakan perulangan for untuk menyimpan data pada array
+	for (int i = 0; i < n; i++)   //Menggunakan perulangan for untuk menyimpan data pada array
 	{
 		cout << "Data Ke-" << (i + 1) << ": ";   //Memasukkan nilai data n kedalam array arr
 	}
+}
+//Procedur Insertoinsort
+void Insertionsort()
+{
 
+	int temp;   //Membuat variable data temoprer atau penyimpanan sementara
+	int j;      //Membuat variable j sebagai penanda
+
+	for (int i = 1; 1 < n; i++)             //1. Looping dengan i dimulai dari 1 hingga n-1
+	{
+		temp = arr[i];                     //2. Simpan nilai arr[i] ke variable sementara temp
+
+		j = i - 1;                         //3. Setting nilai j sama dengan i-1;
+	}
 }
